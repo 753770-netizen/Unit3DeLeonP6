50bd0d574cd2fec450fa54b2333addc31116987a
753770-netizen <753770@student.dusd.net>
2026-04-14 13:52:20 -0700

    on bonus: easy one

--- P6DeLeonUnit3/Assets/Scripts/SpawnManager.cs
+++ P6DeLeonUnit3/Assets/Scripts/SpawnManager.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 spawnPos = new Vector3(20, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    public GameObject[] obstaclePrefabs;
+    private int randomObstacle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,7 +28,8 @@ public class SpawnManager : MonoBehaviour
     {
         if (playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
         }
     }
 }
